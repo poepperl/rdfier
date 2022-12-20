28eb5e0c2b63445f1a259feee04a27456691f488
HerrScrumbleton <scrumbleton97@gmail.com>
2022-12-20 14:10:32 +0100

    Vervollständige Erstellung der Uncertainty Flags

--- src/unco/data/dataset.py
+++ src/unco/data/dataset.py
@@ -1,4 +1,4 @@
-import csv
+import pandas as pd
 import random
 from unco.data import Reader
 from colorama import Fore
@@ -9,25 +9,26 @@ class Dataset:
         Class that adds a perturbation by creating random uncertainty.
     """
 
-    data: csv.reader
+    data: pd.DataFrame
+    uncertainty_flags: dict
 
     def __init__(self, path: str) -> None:
         self.data = Reader(path).read()
 
-    def add_uncertainty_flags(self, number_of_uncertainties: int=0, list_of_columns: list[int] =[]) -> None:
-        first_line = next(self.data)
-        ncolums = len(next(self.data))
+    def add_uncertainty_flags(self, number_of_uncertain_columns: int=0, list_of_columns: list[int] =[], uncertainties_per_column: int = 0) -> None:
+        nrows, ncolums = self.data.shape
 
+        list_of_columns = list(dict.fromkeys(list_of_columns)) # Remove all duplicates from list
 
         if len(list_of_columns) == 0:
             # get random number of uncertainties between 1 and the number of rows - 1
-            if number_of_uncertainties <= 0: # Random number of uncertainty values between 1 and number of rows
-                number_of_uncertainties = random.randrange(1, ncolums)
-            elif number_of_uncertainties >= ncolums:
+            if number_of_uncertain_columns <= 0: # Random number of uncertainty values between 1 and number of rows
+                number_of_uncertain_columns = random.randrange(1, ncolums)
+            elif number_of_uncertain_columns >= ncolums:
                 print(Fore.RED + "ERROR: Number of Uncertanties to high." + Fore.RESET)
                 return
 
-            uncertain_columns = random.sample(range(1, ncolums), number_of_uncertainties)
+            uncertain_columns = random.sample(range(1, ncolums), number_of_uncertain_columns)
 
         else:
             # catch wrong inputs:
@@ -40,7 +41,14 @@ class Dataset:
             
             uncertain_columns = list_of_columns
 
-        # TODO: Für jede Spalte wähle zufällige Anzahl an Uncertainty Values und erstelle eine neue csv in welche diese eingetragen werden.
+        uncertainty_flags = {}
+        for column in uncertain_columns:
+            if uncertainties_per_column < 1 or uncertainties_per_column > nrows-1:
+                uncertain_values = random.sample(range(0, nrows), random.randint(1, nrows)) # Get random row indices
+            else:
+                uncertain_values = random.sample(range(0, nrows), uncertainties_per_column)
+            
+            for row in uncertain_values:
+                uncertainty_flags[(row,column)] = 1
 
-p = Dataset(r"D:\Dokumente\Repositories\unco\tests\test_data\csv_testdata\unittest_reader.csv")
-p.add_uncertainty_flags(3, [])
+        self.uncertainty_flags = uncertainty_flags
