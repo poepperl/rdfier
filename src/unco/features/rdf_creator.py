1795371fa8cd46ad2e8f9d4767f9418c1e4b97bb
HerrScrumbleton <scrumbleton97@gmail.com>
2022-12-29 11:45:16 +0100

    Vorlage RDF_Creator

---
+++ src/unco/features/rdf_creator.py
@@ -0,0 +1,63 @@
+from unco.data.dataset import Dataset
+
+
+class RDFCreator():
+    """
+        Class which creates an RDF-XML file.
+
+    Attributes
+    ----------
+    dataset : Dataset
+        Dataset which contains the data of the rdf graph.
+    """
+
+    dataset: Dataset
+
+    def __init__(self, dataset: Dataset) -> None:
+        """
+        Parameters
+        ----------
+        dataset : Dataset
+            Dataset which contains the data of the rdf graph.
+        """
+        self.dataset = dataset
+
+    def generate_solution_1(self) -> None:
+        """ Method to generate the RDF-XML file of solution 1.
+        """
+        pass #TODO: Generieren des Graphs von Lösung 1
+
+    def generate_solution_2(self) -> None:
+        """ Method to generate the RDF-XML file of solution 2.
+        """
+        pass #TODO: Generieren des Graphs von Lösung 2
+
+    def generate_solution_3(self) -> None:
+        """ Method to generate the RDF-XML file of solution 3.
+        """
+        pass #TODO: Generieren des Graphs von Lösung 3
+
+    def generate_solution_4(self) -> None:
+        """ Method to generate the RDF-XML file of solution 4.
+        """
+        pass #TODO: Generieren des Graphs von Lösung 4
+
+    def generate_solution_5(self) -> None:
+        """ Method to generate the RDF-XML file of solution 5.
+        """
+        pass #TODO: Generieren des Graphs von Lösung 5
+
+    def generate_solution_6(self) -> None:
+        """ Method to generate the RDF-XML file of solution 6.
+        """
+        pass #TODO: Generieren des Graphs von Lösung 6
+
+    def generate_solution_7(self) -> None:
+        """ Method to generate the RDF-XML file of solution 7.
+        """
+        pass #TODO: Generieren des Graphs von Lösung 7
+
+    def generate_solution_8(self) -> None:
+        """ Method to generate the RDF-XML file of solution 8.
+        """
+        pass #TODO: Generieren des Graphs von Lösung 8
