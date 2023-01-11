be3db63f82524a6e655ba1bba2bdf775abea51c9
HerrScrumbleton <scrumbleton97@gmail.com>
2023-01-11 14:57:01 +0100

    RDF_Generator simplified

--- src/unco/features/rdf_generator.py
+++ src/unco/features/rdf_generator.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from cmath import nan
 from typing import Literal
 from unco.data.dataset import Dataset
 from rdflib import Graph, Namespace, BNode, Literal, URIRef
@@ -28,28 +27,20 @@ class RDFGenerator():
         """
         self.dataset = dataset
         self.graph = Graph()
-        self.node_counter = 1
 
         self.graph.bind("nm", NM)
         self.graph.bind("nmo", NMO)
         self.graph.bind("un", UN)
         self.graph.bind("foaf", FOAF)
 
-    def generate_solution_1(self) -> None:
-        """ Method to generate the RDF-XML file of solution 1.
-        """
-        pass #TODO: Generieren des Graphs von Lösung 1
-
-    def generate_solution_2(self) -> None:
-        """ Method to generate the RDF-XML file of solution 2.
+    def generate_solution(self,solution_id):
+        """ Method to generate the RDF-XML file.
         """
         coin_ids = self.dataset.data[self.dataset.data.columns[0]] # Get the list of id's
 
         for row_index, id in enumerate(coin_ids):
-            coin = URIRef("node_" + str(row_index))
-            
-            self.graph.add((coin, NMO.hasObjectType, NM.coin))
-            self.graph.add((coin, FOAF.name, Literal("Coin_" + str(id))))
+            coin = URIRef("Coin_" + str(id))
+            self.graph.add((coin, NMO.hasObjectType, NM.coin)) # Coin_X gets ObjectType Coin
 
             for column_index in range(1, len(self.dataset.data.columns)):
                 if pd.notnull(self.dataset.data.iat[row_index,column_index]): # Check if value isn't NaN
@@ -59,47 +50,72 @@ class RDFGenerator():
                     object = Literal(self.dataset.data.iat[row_index,column_index])
 
                     if column_index in self.dataset.uncertainty_flags:
-                        if row_index in self.dataset.uncertainty_flags[column_index]:
-                            print("Uncertainty at row " + str(row_index) + " and column " + str(column_index))
-                            node = BNode(str(self.dataset.data.columns[column_index]) + self.dataset.data.iat[row_index,column_index])
-                            self.node_counter += 1
-                            self.graph.add((coin, NMO[predicate], node))
-                            self.graph.add((node, UN.hasUncertainty, NM.uncertain_value))
-                            self.graph.add((node, RDF.value, NM[object]))
+                        if row_index in self.dataset.uncertainty_flags[column_index]: # If current value is uncertain, do:
+                            match solution_id:
+                                case 1:
+                                    self._generate_uncertain_value_solution_1()
+                                case 2:
+                                    self._generate_uncertain_value_solution_2(coin, predicate, str(self.dataset.data.columns[column_index]) + self.dataset.data.iat[row_index,column_index])
+                                case 3:
+                                    self._generate_uncertain_value_solution_3()
+                                case 4:
+                                    self._generate_uncertain_value_solution_4()
+                                case 5:
+                                    self._generate_uncertain_value_solution_5()
+                                case 6:
+                                    self._generate_uncertain_value_solution_6()
+                                case 7:
+                                    self._generate_uncertain_value_solution_7()
+                                case 8:
+                                    self._generate_uncertain_value_solution_8()
                             continue
 
-                    self.graph.add((coin, NMO[predicate], NM[object]))
-
-
-    def generate_solution_3(self) -> None:
-        """ Method to generate the RDF-XML file of solution 3.
+                    self.graph.add((coin, NMO[predicate], NM[object])) # Example: Coin_4 hasMaterial ar
+        
+    def _generate_uncertain_value_solution_1(self) -> None:
+        """ Method to create an uncertain value of solution 1.
         """
-        pass #TODO: Generieren des Graphs von Lösung 3
+        pass #TODO: Generieren der unsicheren Werte von Lösung 1
 
-    def generate_solution_4(self) -> None:
-        """ Method to generate the RDF-XML file of solution 4.
+    def _generate_uncertain_value_solution_2(self, coin, predicate, nodename : str) -> None:
+        """ Method to create an uncertain value of solution 2.
         """
-        pass #TODO: Generieren des Graphs von Lösung 4
+        node = BNode(nodename)
 
-    def generate_solution_5(self) -> None:
-        """ Method to generate the RDF-XML file of solution 5.
-        """
-        pass #TODO: Generieren des Graphs von Lösung 5
+        self.graph.add((coin, NMO[predicate], node))
+        self.graph.add((node, UN.hasUncertainty, NM.uncertain_value))
+        self.graph.add((node, RDF.value, NM[object]))
 
-    def generate_solution_6(self) -> None:
-        """ Method to generate the RDF-XML file of solution 6.
-        """
-        pass #TODO: Generieren des Graphs von Lösung 6
 
-    def generate_solution_7(self) -> None:
-        """ Method to generate the RDF-XML file of solution 7.
+    def _generate_uncertain_value_solution_3(self) -> None:
+        """ Method to create an uncertain value of solution 3.
         """
-        pass #TODO: Generieren des Graphs von Lösung 7
+        pass #TODO: Generieren der unsicheren Werte von Lösung 3
 
-    def generate_solution_8(self) -> None:
-        """ Method to generate the RDF-XML file of solution 8.
+    def _generate_uncertain_value_solution_4(self) -> None:
+        """ Method to create an uncertain value of solution 4.
         """
-        pass #TODO: Generieren des Graphs von Lösung 8
+        pass #TODO: Generieren der unsicheren Werte von Lösung 4
+
+    def _generate_uncertain_value_solution_5(self) -> None:
+        """ Method to create an uncertain value of solution 5.
+        """
+        pass #TODO: Generieren der unsicheren Werte von Lösung 5
+
+    def _generate_uncertain_value_solution_6(self) -> None:
+        """ Method to create an uncertain value of solution 6.
+        """
+        pass #TODO: Generieren der unsicheren Werte von Lösung 6
+
+    def _generate_uncertain_value_solution_7(self) -> None:
+        """ Method to create an uncertain value of solution 7.
+        """
+        pass #TODO: Generieren der unsicheren Werte von Lösung 7
+
+    def _generate_uncertain_value_solution_8(self) -> None:
+        """ Method to create an uncertain value of solution 8.
+        """
+        pass #TODO: Generieren der unsicheren Werte von Lösung 8
 
 if __name__ == "__main__":
     dataset = Dataset(r"D:\Dokumente\Repositories\unco\tests\test_data\csv_testdata\cointest_5.csv")
@@ -108,7 +124,8 @@ if __name__ == "__main__":
     generator = RDFGenerator(dataset)
 
     # generator.generate_solution_1()
-    generator.generate_solution_2()
+    generator.generate_solution(2)
+    print(generator.graph.serialize())
     # generator.generate_solution_6()
     # generator.generate_solution_7()
     
