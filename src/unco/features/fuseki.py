98baef716dce12764de5067bbbab6cb7623f818a
HerrScrumbleton <scrumbleton97@gmail.com>
2023-02-06 11:49:22 +0100

    UNCO-4: Fuseki Doku und Feinschliff

--- src/unco/features/fuseki.py
+++ src/unco/features/fuseki.py
@@ -1,36 +1,28 @@
-from unco import UNCO_PATH
 import os
 import subprocess
 import psutil
 import socket
 import time
 import requests
-from SPARQLWrapper import SPARQLWrapper
+from unco import UNCO_PATH
 from colorama import Fore
 
 class FusekiServer:
     """
-        Class that starts a server on which the requests are tested
-
-    Attributes
-    ----------
-    data : pd.DataFrame
-        DataFrame wich includes the data from Reader.
+        Interface to the fuseki server.
     """
 
-    def __init__(self, fuseki_path: str = os.path.join(UNCO_PATH, r"src\fuseki"), mb_ram: int = 8200, data_path: str = None) -> None:
+    def __init__(self, fuseki_path: str = os.path.join(UNCO_PATH, r"src\fuseki"), gb_ram: int = 4) -> None:
         """
         Parameters
         ----------
         fuseki_path : str
             Path to the fuseki server folder.
         mb_ram : int
-            RAM size in MB for the fuseki server.
-        data_path : str
-            Path to the data which should be uploaded.
+            RAM size in GB for the fuseki server.
         """
         self.FUSEKI_PATH = fuseki_path
-        self.MB_RAM = mb_ram
+        self.GB_RAM = gb_ram
         self.server = None
         self.starter_path = os.path.join(UNCO_PATH, r"src\unco\features\server_starter.bat")
         self._initialize()
@@ -42,11 +34,11 @@ class FusekiServer:
         """
         with open(self.starter_path, 'w') as starter:
             starter.write("echo 'Running fuseki server'\ncd \"" + self.FUSEKI_PATH + "\"\n")
-            starter.write("java -Xmx" + str(self.MB_RAM) + "M -jar \"fuseki-server.jar\" --update --mem /ds %*")
+            starter.write("java -Xmx" + str(self.GB_RAM) + "G -jar \"fuseki-server.jar\" --update --mem /ds %*")
 
     def start_server(self) -> None:
         """
-            Method, which starts the fuseki server
+            Method, which starts the fuseki server.
         """
         if self.server:
             print(Fore.YELLOW + "WARNING: Server is already running." + Fore.RESET)
@@ -56,34 +48,44 @@ class FusekiServer:
             time.sleep(3)
     
     def upload_data(self, path: str) -> None:
+        """
+            Method, which uploads data in rdf or turtle files to the server
+
+        Parameters
+        ----------
+        path : str
+            Path to the file that should be uploaded.
+        """
         if self.server == None:
-            print(Fore.YELLOW + "WARNING: Server is shut down. Please start the server." + Fore.RESET)
+            print(Fore.RED + "ERROR: Server is shut down. Please start the server." + Fore.RESET)
             return
         if path[-3:] == "rdf":
             headers = {'Content-Type': r'application/rdf+xml;charset=utf-8', 'Filename' : path}
-        elif path[-3:] == "xml":
-            headers = {'Content-Type': r'text/xml;charset=utf-8'}
         elif path[-3:] == "ttl":
-            headers = {'Content-Type': r'application/octet-stream;charset=utf-8'}
-        data = open(r'D:\Dokumente\Repositories\unco\data\output\7.rdf', 'r', encoding='utf-8').read()
+            headers = {'Content-Type': r'text/turtle;charset=utf-8'}
+        else:
+            print(Fore.RED + "ERROR: Unknown Datatyp. Please use \".rdf\" or \".ttl\" files as input." + Fore.RESET)
+            return
+        data = open(path, 'r', encoding='utf-8').read()
         newdata = requests.post('http://localhost:3030/ds/data', data=data.encode('utf-8'), headers=headers)
 
 
     def stop_server(self) -> None:
         """
-            Method, which stops the fuseki server
+            Method, which stops the fuseki server.
         """
         if self.server:
             for child in psutil.Process(self.server.pid).children():
                 child.kill()
             self.server = None
         else:
-            print(Fore.YELLOW + "WARNING: There is no server to stop." + Fore.RESET)
+            print(Fore.YELLOW + "WARNING: Server already stopped." + Fore.RESET)
 
 if __name__ == "__main__":
     f = FusekiServer()
     f.upload_data(r"D:\Dokumente\Repositories\unco\data\output\7.rdf")
-    # f.stop_server()
+    input()
+    f.stop_server()
 
 
     # response = requests.post('http://localhost:3030/ds/sparql', data={'query': 'SELECT * WHERE {?sub ?pred ?obj .} LIMIT 10'})
--- src/unco/features/server_starter.bat
+++ src/unco/features/server_starter.bat
@@ -1,3 +1,3 @@
 echo 'Running fuseki server'
 cd "d:\dokumente\repositories\unco\src\fuseki"
-java -Xmx8200M -jar "fuseki-server.jar" --update --mem /ds %*
+java -Xmx4G -jar "fuseki-server.jar" --update --mem /ds %*
