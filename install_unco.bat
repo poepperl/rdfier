00fc5b943a948284eb6a0b210c9d129c9184db87
HerrScrumbleton <scrumbleton97@gmail.com>
2023-03-25 16:13:46 +0100

    Delete wrong batch files

--- install_unco.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo "Install requirements..."
-cd "C:\Users\scrum\Documents\Repositories\unco"
-.venv\Scripts\activate & pip install -r requirements.txt
-PAUSE
--- start_server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd "C:\Users\scrum\Documents\Repositories\unco"
-.venv\Scripts\activate & streamlit run src/app/Startseite.py
