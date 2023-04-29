539bdff6b63e34d356a19e6042bbb082ea305b25
HerrScrumbleton <scrumbleton97@gmail.com>
2023-04-29 16:13:07 +0200

    Add model selection and try to clean and minimize

--- src/app/RDFier.py
+++ src/app/RDFier.py
@@ -11,23 +11,27 @@ st.set_page_config(
     page_title="RDFier",
     layout="wide")
 
-if "rdfdata" not in st.session_state:
-    st.session_state.rdfdata = None
+# if "rdfdata" not in st.session_state:
+#     st.session_state.rdfdata = None
 
-if "generate" not in st.session_state:
-    st.session_state.generate = False
+# if "generate" not in st.session_state:
+#     st.session_state.generate = False
 
 if "uploaded" not in st.session_state:
     st.session_state.uploaded = False
 
-if "turtle" not in st.session_state:
-    st.session_state.turtle = None
+# if "turtle" not in st.session_state:
+#     st.session_state.turtle = None
 
-if "generator" not in st.session_state:
-    st.session_state.generator = None
+# if "generator" not in st.session_state:
+#     st.session_state.generator = None
 
 if "dataframe" not in st.session_state:
     st.session_state.dataframe = None
+
+def update():
+    st.session_state.rdfdata = RDFData(st.session_state.dataframe.copy())
+    st.session_state.turtle = None
 
 # Begin webpage---------------------------------------------------------------------------
 
@@ -36,39 +40,31 @@ st.subheader("A RDF Mapper")
 
 uploaded_file = st.file_uploader("Upload", type=["csv"], accept_multiple_files=False)
 
-if uploaded_file and not st.session_state.uploaded:
-    st.session_state.dataframe = pd.read_csv(uploaded_file)
-    st.session_state.rdfdata = RDFData(st.session_state.dataframe.copy())
-    st.session_state.uploaded = True
-
-elif st.session_state.uploaded and not uploaded_file:
-    st.session_state.uploaded = False
+if not uploaded_file:
     st.session_state.generate = False
     st.session_state.rdfdata = None
     st.session_state.turtle = None
     st.session_state.generator = None
 
-if st.session_state.rdfdata is not None:
-
-    dataframe = st.experimental_data_editor(st.session_state.dataframe)
-    
-    if not dataframe.equals(st.session_state.dataframe):
-        st.session_state.rdfdata = RDFData(dataframe.copy())
-        st.session_state.dataframe = dataframe
-        st.session_state.turtle = None
+else:
+    st.session_state.dataframe = st.experimental_data_editor(pd.read_csv(uploaded_file), on_change=update)
+    if st.session_state.rdfdata is None:
+        update()
 
     with st.container():
         col1, col2 = st.columns(2)
 
-        xml_format = col1.radio("RDF Format", ("Turtle", "XML"))
+        xml_format = col1.radio("RDF format", ("Turtle", "XML"))
 
-        graphical_version = col2.checkbox("Graphische Darstellung", value=True)
+        graphical_version = col2.checkbox("Show graphical version", value=True)
 
-    uploaded_prefixes = st.file_uploader("Präfixtabelle", type=["csv"], accept_multiple_files=False)
+        solution = col2.selectbox("Select model:", (1,2,3,4,5,6,7,8))
+
+    uploaded_prefixes = st.file_uploader("Prefixes", type=["csv"], accept_multiple_files=False)
 
     # Graph generieren-------------------------------------------------------------------------------
 
-    generate = st.button("RDF Graph generieren")
+    generate = st.button("Generate RDF graph")
 
     if generate:
         st.session_state.generate = True
@@ -77,11 +73,9 @@ if st.session_state.rdfdata is not None:
     if st.session_state.generate:
         if (xml_format == "Turtle") != st.session_state.turtle:
             st.session_state.data_state = (xml_format == "Turtle")
-            rdf_data = st.session_state.rdfdata
-
-            st.session_state.generator = GraphGenerator(rdf_data)
+            st.session_state.generator = GraphGenerator(st.session_state.rdfdata)
             st.session_state.generator.load_prefixes(pd.read_csv(uploaded_prefixes))
-            st.session_state.generator.generate_solution(xml_format=(xml_format=="XML"))
+            st.session_state.generator.generate_solution(solution_id=solution,xml_format=(xml_format=="XML"))
 
         
         path = Path(UNCO_PATH, "data/output/graph" + (".ttl" if xml_format=="Turtle" else ".rdf"))
