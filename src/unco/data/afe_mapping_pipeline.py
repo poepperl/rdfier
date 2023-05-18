d32a3adc14c697cbe997e5c57e5de406e6741e41
HerrScrumbleton <scrumbleton97@gmail.com>
2023-05-18 10:18:19 +0200

    Updated with large datasets

--- src/unco/data/afe_mapping_pipeline.py
+++ src/unco/data/afe_mapping_pipeline.py
@@ -1,167 +1,167 @@
 import pandas as pd
 from pathlib import Path
 
-# def change_afe_coin_id(dataframe : pd.DataFrame) -> pd.DataFrame:
-#     """
-#         Turns the ids in column "Coin^^uri" into "afe:"+str(id)
+def change_afe_coin_id(dataframe : pd.DataFrame) -> pd.DataFrame:
+    """
+        Turns the ids in column "Coin^^uri" into "afe:"+str(id)
     
-#         Parameters:
-#         -----------
-#         dataframe : pd.DataFrame
-#             Dataframe which should be updated
-#     """
-#     dataframe["Coin^^uri"] = dataframe["Coin^^uri"].apply(lambda x: "afe:"+str(x) if pd.notna(x) else pd.NA)
-#     return dataframe
+        Parameters:
+        -----------
+        dataframe : pd.DataFrame
+            Dataframe which should be updated
+    """
+    dataframe["Coin^^uri"] = dataframe["Coin^^uri"].apply(lambda x: "afe:"+str(x) if pd.notna(x) else pd.NA)
+    return dataframe
 
-# def change_findspot(dataframe : pd.DataFrame) -> pd.DataFrame:
-#     """
-#         Turns the urls in column "nmo:hasFindspot^^uri" into "gaz:"+str(id)
+def change_findspot(dataframe : pd.DataFrame) -> pd.DataFrame:
+    """
+        Turns the urls in column "nmo:hasFindspot^^uri" into "gaz:"+str(id)
     
-#         Parameters:
-#         -----------
-#         dataframe : pd.DataFrame
-#             Dataframe which should be updated
-#     """
-#     dataframe["nmo:hasFindspot^^uri"] = dataframe["nmo:hasFindspot^^uri"].apply(lambda x: pd.NA if pd.isna(x) else "gaz:" + str(x)[-7:])
-#     return dataframe
+        Parameters:
+        -----------
+        dataframe : pd.DataFrame
+            Dataframe which should be updated
+    """
+    dataframe["nmo:hasFindspot^^uri"] = dataframe["nmo:hasFindspot^^uri"].apply(lambda x: pd.NA if pd.isna(x) else "gaz:" + str(x)[-7:])
+    return dataframe
 
-# def turn_nomisma_values_to_uris(dataframe : pd.DataFrame) -> pd.DataFrame:
-#     """
-#         Turns the values in nomisma columns "nm:"+str(value)
+def turn_nomisma_values_to_uris(dataframe : pd.DataFrame) -> pd.DataFrame:
+    """
+        Turns the values in nomisma columns "nm:"+str(value)
     
-#         Parameters:
-#         -----------
-#         dataframe : pd.DataFrame
-#             Dataframe which should be updated
-#     """
-#     dataframe["nmo:hasMaterial^^uri"] = dataframe["nmo:hasMaterial^^uri"].apply(lambda x: "nm:"+str(x) if pd.notna(x) else pd.NA)
-#     dataframe["nmo:hasMint^^uri"] = dataframe["nmo:hasMint^^uri"].apply(lambda x: "nm:"+str(x) if pd.notna(x) else pd.NA)
-#     dataframe["nmo:hasMint2^^uri"] = dataframe["nmo:hasMint2^^uri"].apply(lambda x: "nm:"+str(x) if pd.notna(x) else pd.NA)
-#     dataframe["nmo:hasDenomination^^uri"] = dataframe["nmo:hasDenomination^^uri"].apply(lambda x: "nm:"+str(x) if pd.notna(x) else pd.NA)
-#     dataframe["nmo:hasDenomination2^^uri"] = dataframe["nmo:hasDenomination2^^uri"].apply(lambda x: "nm:"+str(x) if pd.notna(x) else pd.NA)
+        Parameters:
+        -----------
+        dataframe : pd.DataFrame
+            Dataframe which should be updated
+    """
+    dataframe["nmo:hasMaterial^^uri"] = dataframe["nmo:hasMaterial^^uri"].apply(lambda x: "nm:"+str(x) if pd.notna(x) else pd.NA)
+    dataframe["nmo:hasMint^^uri"] = dataframe["nmo:hasMint^^uri"].apply(lambda x: "nm:"+str(x) if pd.notna(x) else pd.NA)
+    dataframe["nmo:hasMint2^^uri"] = dataframe["nmo:hasMint2^^uri"].apply(lambda x: "nm:"+str(x) if pd.notna(x) else pd.NA)
+    dataframe["nmo:hasDenomination^^uri"] = dataframe["nmo:hasDenomination^^uri"].apply(lambda x: "nm:"+str(x) if pd.notna(x) else pd.NA)
+    dataframe["nmo:hasDenomination2^^uri"] = dataframe["nmo:hasDenomination2^^uri"].apply(lambda x: "nm:"+str(x) if pd.notna(x) else pd.NA)
 
-#     return dataframe
+    return dataframe
 
-# def combine_mint_and_denomination(dataframe : pd.DataFrame) -> pd.DataFrame:
-#     """
-#         Combines the two mint and denomination columns.
+def combine_mint_and_denomination(dataframe : pd.DataFrame) -> pd.DataFrame:
+    """
+        Combines the two mint and denomination columns.
     
-#         Parameters:
-#         -----------
-#         dataframe : pd.DataFrame
-#             Dataframe which should be updated
-#     """
-#     dataframe["nmo:hasMint^^uri"] = dataframe[["nmo:hasMint^^uri","nmo:hasMint2^^uri"]].apply(lambda x: "; ".join(x) if pd.notna(x[1]) and pd.notna(x[0]) else x[0], axis =1)
-#     dataframe["nmo:hasDenomination^^uri"] = dataframe[["nmo:hasDenomination^^uri","nmo:hasDenomination2^^uri"]].apply(lambda x: "; ".join(x) if pd.notna(x[1]) and pd.notna(x[0]) else x[0], axis =1)
+        Parameters:
+        -----------
+        dataframe : pd.DataFrame
+            Dataframe which should be updated
+    """
+    dataframe["nmo:hasMint^^uri"] = dataframe[["nmo:hasMint^^uri","nmo:hasMint2^^uri"]].apply(lambda x: "; ".join(x) if pd.notna(x[1]) and pd.notna(x[0]) else x[0], axis =1)
+    dataframe["nmo:hasDenomination^^uri"] = dataframe[["nmo:hasDenomination^^uri","nmo:hasDenomination2^^uri"]].apply(lambda x: "; ".join(x) if pd.notna(x[1]) and pd.notna(x[0]) else x[0], axis =1)
 
-#     return dataframe.drop(["nmo:hasMint2^^uri","nmo:hasDenomination2^^uri"],axis=1)
+    return dataframe.drop(["nmo:hasMint2^^uri","nmo:hasDenomination2^^uri"],axis=1)
 
-# def simplify_all_id_columns(dataframe : pd.DataFrame) -> pd.DataFrame:
-#     """
-#         All id values will be replaced with "1".
+def simplify_all_id_columns(dataframe : pd.DataFrame) -> pd.DataFrame:
+    """
+        All id values will be replaced with "1".
     
-#         Parameters:
-#         -----------
-#         dataframe : pd.DataFrame
-#             Dataframe which should be updated
-#     """
-#     dataframe["nmo:hasDie^^id**1"] = dataframe["nmo:hasDie^^id**1"].apply(lambda x: str(1) if pd.notna(x) else x)
-#     dataframe["nmo:hasObverse^^id**2"] = dataframe["nmo:hasObverse^^id**2"].apply(lambda x: str(1) if pd.notna(x) else x)
-#     dataframe["nmo:hasReverse^^id**3"] = dataframe["nmo:hasReverse^^id**3"].apply(lambda x: str(1) if pd.notna(x) else x)
+        Parameters:
+        -----------
+        dataframe : pd.DataFrame
+            Dataframe which should be updated
+    """
+    dataframe["nmo:hasDie^^id**1"] = dataframe["nmo:hasDie^^id**1"].apply(lambda x: str(1) if pd.notna(x) else x)
+    dataframe["nmo:hasObverse^^id**2"] = dataframe["nmo:hasObverse^^id**2"].apply(lambda x: str(1) if pd.notna(x) else x)
+    dataframe["nmo:hasReverse^^id**3"] = dataframe["nmo:hasReverse^^id**3"].apply(lambda x: str(1) if pd.notna(x) else x)
 
-#     return dataframe
+    return dataframe
 
-# def remove_wrong_context_from_obverse(dataframe : pd.DataFrame) -> pd.DataFrame:
-#     """
-#         Removes reverse context from obverse.
+def remove_wrong_context_from_obverse(dataframe : pd.DataFrame) -> pd.DataFrame:
+    """
+        Removes reverse context from obverse.
     
-#         Parameters:
-#         -----------
-#         dataframe : pd.DataFrame
-#             Dataframe which should be updated
-#     """
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Vs.: ")[-1] if pd.notna(x) else pd.NA)
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Obv.: ")[-1] if pd.notna(x) else pd.NA)
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Av.: ")[-1] if pd.notna(x) else pd.NA)
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Obv: ")[-1] if pd.notna(x) else pd.NA)
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Ob: ")[-1] if pd.notna(x) else pd.NA)
+        Parameters:
+        -----------
+        dataframe : pd.DataFrame
+            Dataframe which should be updated
+    """
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Vs.: ")[-1] if pd.notna(x) else pd.NA)
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Obv.: ")[-1] if pd.notna(x) else pd.NA)
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Av.: ")[-1] if pd.notna(x) else pd.NA)
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Obv: ")[-1] if pd.notna(x) else pd.NA)
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Ob: ")[-1] if pd.notna(x) else pd.NA)
 
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Rev.: ")[0] if pd.notna(x) else pd.NA)
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Rs.: ")[0] if pd.notna(x) else pd.NA)
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Rev: ")[0] if pd.notna(x) else pd.NA)
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Revers: ")[0] if pd.notna(x) else pd.NA)
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Rev.: ")[0] if pd.notna(x) else pd.NA)
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Rs.: ")[0] if pd.notna(x) else pd.NA)
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Rev: ")[0] if pd.notna(x) else pd.NA)
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).split("Revers: ")[0] if pd.notna(x) else pd.NA)
 
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: pd.NA if "Rs." in str(x) or pd.isna(x) else str(x))
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: pd.NA if "Res." in str(x) or pd.isna(x) else str(x))
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: pd.NA if "Rs." in str(x) or pd.isna(x) else str(x))
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: pd.NA if "Res." in str(x) or pd.isna(x) else str(x))
 
-#     # dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: "".join(str(x).splitlines()) if pd.notna(x) else pd.NA)
-#     dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).replace(";", "|") if pd.notna(x) else pd.NA)
-#     return dataframe
+    # dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: "".join(str(x).splitlines()) if pd.notna(x) else pd.NA)
+    dataframe["2__nmo:hasContext"] = dataframe["2__nmo:hasContext"].apply(lambda x: str(x).replace(";", "|") if pd.notna(x) else pd.NA)
+    return dataframe
 
 
-# def remove_wrong_context_from_reverse(dataframe : pd.DataFrame) -> pd.DataFrame:
-#     """
-#         Removes obverse context from reverse.
+def remove_wrong_context_from_reverse(dataframe : pd.DataFrame) -> pd.DataFrame:
+    """
+        Removes obverse context from reverse.
     
-#         Parameters:
-#         -----------
-#         dataframe : pd.DataFrame
-#             Dataframe which should be updated
-#     """
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Rev.: ")[-1] if pd.notna(x) else pd.NA)
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Rs.: ")[-1] if pd.notna(x) else pd.NA)
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Rev: ")[-1] if pd.notna(x) else pd.NA)
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Revers: ")[-1] if pd.notna(x) else pd.NA)
+        Parameters:
+        -----------
+        dataframe : pd.DataFrame
+            Dataframe which should be updated
+    """
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Rev.: ")[-1] if pd.notna(x) else pd.NA)
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Rs.: ")[-1] if pd.notna(x) else pd.NA)
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Rev: ")[-1] if pd.notna(x) else pd.NA)
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Revers: ")[-1] if pd.notna(x) else pd.NA)
 
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Vs.: ")[0] if pd.notna(x) else pd.NA)
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Obv.: ")[0] if pd.notna(x) else pd.NA)
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Av.: ")[0] if pd.notna(x) else pd.NA)
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Obv: ")[0] if pd.notna(x) else pd.NA)
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Ob: ")[0] if pd.notna(x) else pd.NA)
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Vs.: ")[0] if pd.notna(x) else pd.NA)
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Obv.: ")[0] if pd.notna(x) else pd.NA)
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Av.: ")[0] if pd.notna(x) else pd.NA)
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Obv: ")[0] if pd.notna(x) else pd.NA)
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).split("Ob: ")[0] if pd.notna(x) else pd.NA)
 
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: pd.NA if "Ob." in str(x) or pd.isna(x) else str(x))
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: pd.NA if "Obv." in str(x) or pd.isna(x) else str(x))
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: pd.NA if "Ob." in str(x) or pd.isna(x) else str(x))
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: pd.NA if "Obv." in str(x) or pd.isna(x) else str(x))
 
-#     # dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: "".join(str(x).splitlines()) if pd.notna(x) else pd.NA)
-#     dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).replace(";", "|") if pd.notna(x) else pd.NA)
-#     return dataframe
+    # dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: "".join(str(x).splitlines()) if pd.notna(x) else pd.NA)
+    dataframe["3__nmo:hasContext"] = dataframe["3__nmo:hasContext"].apply(lambda x: str(x).replace(";", "|") if pd.notna(x) else pd.NA)
+    return dataframe
 
 
-# def change_gYear_format(dataframe : pd.DataFrame) -> pd.DataFrame:
-#     """
-#         Takes float entries of gyear columns and change them to int.
+def change_gYear_format(dataframe : pd.DataFrame) -> pd.DataFrame:
+    """
+        Takes float entries of gyear columns and change them to int.
     
-#         Parameters:
-#         -----------
-#         dataframe : pd.DataFrame
-#             Dataframe which should be updated
-#     """
-#     col_list = [col for col in dataframe.columns if "^^xsd:gYear" in str(col)]
-#     for column in col_list:
-#         dataframe[column] = dataframe[column].apply(lambda x: int(x) if pd.notna(x) else pd.NA)
+        Parameters:
+        -----------
+        dataframe : pd.DataFrame
+            Dataframe which should be updated
+    """
+    col_list = [col for col in dataframe.columns if "^^xsd:gYear" in str(col)]
+    for column in col_list:
+        dataframe[column] = dataframe[column].apply(lambda x: int(x) if pd.notna(x) else pd.NA)
 
-#     return dataframe
+    return dataframe
 
 if __name__ == "__main__":
     from unco import UNCO_PATH
     from unco.data.rdf_data import RDFData
     from unco.features.graph_generator import GraphGenerator
 
-    # dataframe = pd.read_csv(Path(UNCO_PATH,r"tests\testdata\afe\afemapping_1_public_withoutUncertainties.csv"))
+    dataframe = pd.read_csv(Path(UNCO_PATH,r"tests\testdata\afe\afemapping_1_public_withoutUncertainties.csv"))
 
-    # dataframe = change_afe_coin_id(dataframe)
-    # dataframe = change_findspot(dataframe)
-    # dataframe = turn_nomisma_values_to_uris(dataframe)
-    # dataframe = combine_mint_and_denomination(dataframe)
-    # dataframe = remove_wrong_context_from_obverse(dataframe)
-    # dataframe = remove_wrong_context_from_reverse(dataframe)
-    # dataframe = simplify_all_id_columns(dataframe)
-    # dataframe = change_gYear_format(dataframe)
+    dataframe = change_afe_coin_id(dataframe)
+    dataframe = change_findspot(dataframe)
+    dataframe = turn_nomisma_values_to_uris(dataframe)
+    dataframe = combine_mint_and_denomination(dataframe)
+    dataframe = remove_wrong_context_from_obverse(dataframe)
+    dataframe = remove_wrong_context_from_reverse(dataframe)
+    dataframe = simplify_all_id_columns(dataframe)
+    dataframe = change_gYear_format(dataframe)
 
-    # print(dataframe)
-    # dataframe.to_csv(Path(UNCO_PATH,r"tests\testdata\afe\afemapping_1_public_changed.csv"),index=False)
+    print(dataframe)
+    dataframe.to_csv(Path(UNCO_PATH,r"tests\testdata\afe\afemapping_1_public_changed.csv"),index=False)
 
-    rdf_data = RDFData(pd.read_csv(Path(UNCO_PATH,r"tests\testdata\afe\afemapping_1_public_changed.csv")))
+    rdf_data = RDFData(dataframe)
 
     print(len(rdf_data.types_and_languages), (12,16) in rdf_data.types_and_languages)
 
@@ -169,6 +169,6 @@ if __name__ == "__main__":
 
     gg.load_prefixes(str(Path(UNCO_PATH,r"tests\testdata\afe\namespaces.csv")))
 
-    gg.generate_solution()
+    gg.generate_solution(xml_format=False)
 
     # python -u -m trace -t afe_mapping_pipeline.py
