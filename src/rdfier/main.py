67b1e8321f4e9115c0aec2513b15a54ac0041d47
HerrScrumbleton <scrumbleton97@gmail.com>
2023-10-23 20:31:15 +0200

    Update dependencies

--- src/rdfier/main.py
+++ src/rdfier/main.py
@@ -4,7 +4,8 @@ from pathlib import Path
 from streamlit.web import cli as stcli
 
 def main():
-    sys.argv = ["streamlit", "run", str(Path(RDFIER_PATH, "src/rdfier_app/RDFier.py"))]
+    print("Start RDFier...")
+    sys.argv = ["streamlit", "run", str(Path(RDFIER_PATH, "src/rdfier_app/RDFier.py")), "&>/content/logs.txt", "&"]
     sys.exit(stcli.main())
 
 if __name__ == '__main__':
